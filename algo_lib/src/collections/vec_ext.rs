1860b96d263682f31f41ca517f796c5e3df7cb91
Egor Kulikov <egor@egork.net>
2021-12-28 14:09:18 +0100

    Library

--- algo_lib/src/collections/vec_ext.rs
+++ algo_lib/src/collections/vec_ext.rs
@@ -94,6 +94,30 @@ macro_rules! compress {
     } }
 }
 
+pub trait Detuple {
+    type Res;
+
+    fn detuple(self) -> Self::Res;
+}
+
+impl<A, B, C, D> Detuple for Vec<(A, B, C, D)> {
+    type Res = (Vec<A>, Vec<B>, Vec<C>, Vec<D>);
+
+    fn detuple(self) -> Self::Res {
+        let mut a = Vec::with_capacity(self.len());
+        let mut b = Vec::with_capacity(self.len());
+        let mut c = Vec::with_capacity(self.len());
+        let mut d = Vec::with_capacity(self.len());
+        for (aa, bb, cc, dd) in self {
+            a.push(aa);
+            b.push(bb);
+            c.push(cc);
+            d.push(dd);
+        }
+        (a, b, c, d)
+    }
+}
+
 pub trait IncDec {
     #[must_use]
     fn inc_by_one(self) -> Self;
