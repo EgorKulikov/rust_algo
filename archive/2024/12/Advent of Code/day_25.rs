745d224de1d66b60c451c349c6a315a4098a9060
Egor Kulikov <egor@egork.net>
2024-12-25 06:05:33 +0100

    1 day of advent

---
+++ archive/2024/12/Advent of Code/day_25.rs
@@ -0,0 +1,96 @@
+//{"name":"day_25","group":"Manual","url":"","interactive":false,"timeLimit":2000,"tests":[{"input":"","output":""},{"input":"","output":""}],"testType":"single","input":{"type":"stdin","fileName":null,"pattern":null},"output":{"type":"stdout","fileName":null,"pattern":null},"languages":{"java":{"taskClass":"day_25"}}}
+
+use algo_lib::collections::slice_ext::indices::Indices;
+use algo_lib::io::input::Input;
+use algo_lib::io::output::Output;
+use algo_lib::misc::test_type::TaskType;
+
+use algo_lib::misc::test_type::TestType;
+use algo_lib::string::str::StrReader;
+
+type PreCalc = ();
+
+fn solve(input: &mut Input, out: &mut Output, _test_case: usize, _data: &mut PreCalc) {
+    let mut data = Vec::new();
+
+    while !input.is_empty() {
+        let mut key_lock = Vec::new();
+        while !input.is_exhausted() {
+            let line = input.read_line();
+            if line.is_empty() {
+                break;
+            }
+            key_lock.push(line);
+        }
+        data.push(key_lock);
+    }
+
+    let mut ans = 0;
+    for i in data.indices() {
+        for j in 0..i {
+            let mut good = true;
+            'outer: for k in data[i].indices() {
+                for l in data[i][k].indices() {
+                    if data[i][k][l] == b'#' && data[j][k][l] == b'#' {
+                        good = false;
+                        break 'outer;
+                    }
+                }
+            }
+            if good {
+                ans += 1;
+            }
+        }
+    }
+    out.print_line(ans);
+}
+
+pub static TEST_TYPE: TestType = TestType::Single;
+pub static TASK_TYPE: TaskType = TaskType::Classic;
+
+pub(crate) fn run(mut input: Input, mut output: Output) -> bool {
+    let mut pre_calc = ();
+
+    match TEST_TYPE {
+        TestType::Single => solve(&mut input, &mut output, 1, &mut pre_calc),
+        TestType::MultiNumber => {
+            let t = input.read();
+            for i in 1..=t {
+                solve(&mut input, &mut output, i, &mut pre_calc);
+            }
+        }
+        TestType::MultiEof => {
+            let mut i = 1;
+            while input.peek().is_some() {
+                solve(&mut input, &mut output, i, &mut pre_calc);
+                i += 1;
+            }
+        }
+    }
+    output.flush();
+    match TASK_TYPE {
+        TaskType::Classic => input.is_empty(),
+        TaskType::Interactive => true,
+    }
+}
+
+//START MAIN
+#[cfg(feature = "local")]
+mod tester;
+
+#[cfg(feature = "local")]
+fn main() {
+    tester::run_tests();
+}
+
+#[cfg(not(feature = "local"))]
+fn main() {
+    let mut sin = std::io::stdin();
+    let input = algo_lib::io::input::Input::new(&mut sin);
+
+    let mut stdout = std::io::stdout();
+    let output = algo_lib::io::output::Output::new(&mut stdout);
+
+    run(input, output);
+}
+//END MAIN
