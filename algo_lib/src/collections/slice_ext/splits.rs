334c90ef9859652222fb09d7479fd0f26161bbb8
Egor Kulikov <egor@egork.net>
2026-06-21 11:55:12 +0200

    (feature) two_mut returns two mutable references

--- algo_lib/src/collections/slice_ext/splits.rs
+++ algo_lib/src/collections/slice_ext/splits.rs
@@ -1,10 +1,10 @@
 pub trait Split<T> {
-    fn two_mut(&mut self, i: usize, j: usize) -> (&mut T, &T);
+    fn two_mut(&mut self, i: usize, j: usize) -> (&mut T, &mut T);
     fn three_mut(&mut self, i: usize, j: usize, k: usize) -> (&mut T, &T, &T);
 }
 
 impl<T> Split<T> for [T] {
-    fn two_mut(&mut self, i: usize, j: usize) -> (&mut T, &T) {
+    fn two_mut(&mut self, i: usize, j: usize) -> (&mut T, &mut T) {
         assert_ne!(i, j);
         if i < j {
             let (left, right) = self.split_at_mut(j);
