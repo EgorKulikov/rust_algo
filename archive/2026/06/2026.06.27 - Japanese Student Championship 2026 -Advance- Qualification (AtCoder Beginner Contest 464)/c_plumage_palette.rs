3d1085d79f83176e75109fe3d3d26918147f2f26
Egor Kulikov <egor@egork.net>
2026-06-28 13:54:16 +0200

    (contest) Japanese Student Championship 2026 -Advance- Qualification (AtCoder Beginner Contest 464)

---
+++ archive/2026/06/2026.06.27 - Japanese Student Championship 2026 -Advance- Qualification (AtCoder Beginner Contest 464)/c_plumage_palette.rs
@@ -0,0 +1,78 @@
+use algo_lib::collections::iter_ext::iter_copied::ItersCopied;
+use algo_lib::collections::multi_set::MultiHashSet;
+use algo_lib::io::input::Input;
+use algo_lib::io::output::BoolOutput;
+use algo_lib::io::output::Output;
+use algo_lib::misc::test_type::TaskType;
+use algo_lib::misc::test_type::TestType;
+
+type PreCalc = ();
+
+fn solve(input: &mut Input, out: &mut Output, _test_case: usize, _data: &mut PreCalc) {
+    let n = input.read_size();
+    let m = input.read_size();
+    let adb = input.read_vec::<(usize, usize, usize)>(n);
+
+    let mut colors = MultiHashSet::new();
+    let mut changes = vec![Vec::new(); m];
+    for (a, d, b) in adb {
+        colors.insert(a);
+        changes[d - 1].push((a, b));
+    }
+    for i in 0..m {
+        for (a, b) in changes[i].copy_iter() {
+            colors.remove(&a);
+            colors.insert(b);
+        }
+        out.print_line(colors.num_distinct());
+    }
+}
+
+pub static TEST_TYPE: TestType = TestType::Single;
+pub static TASK_TYPE: TaskType = TaskType::Classic;
+
+pub(crate) fn run(mut input: Input, mut output: Output) -> bool {
+    eprint!("\x1B[33m\x1B[03m");
+    let mut pre_calc = ();
+    output.set_bool_output(BoolOutput::YesNo);
+
+    match TEST_TYPE {
+        TestType::Single => solve(&mut input, &mut output, 1, &mut pre_calc),
+        TestType::MultiNumber => {
+            let t = input.read();
+            for i in 1..=t {
+                solve(&mut input, &mut output, i, &mut pre_calc);
+            }
+        }
+        TestType::MultiEof => {
+            let mut i = 1;
+            while input.peek().is_some() {
+                solve(&mut input, &mut output, i, &mut pre_calc);
+                i += 1;
+            }
+        }
+        _ => {
+            unreachable!();
+        }
+    }
+    eprint!("\x1B[0m");
+    output.flush();
+    input.check_empty()
+}
+
+#[cfg(feature = "local")]
+mod tester;
+
+#[cfg(feature = "local")]
+fn main() {
+    tester::run_tests();
+}
+
+#[cfg(not(feature = "local"))]
+fn main() {
+    #[cfg(debug_assertions)]
+    eprintln!("Library code is available at https://github.com/EgorKulikov/rust_algo");
+    let input = algo_lib::io::input::Input::stdin();
+    let output = algo_lib::io::output::Output::stdout();
+    run(input, output);
+}
---
+++ archive/2026/06/2026.06.27 - Japanese Student Championship 2026 -Advance- Qualification (AtCoder Beginner Contest 464)/d_celester.rs
@@ -0,0 +1,83 @@
+use algo_lib::io::input::Input;
+use algo_lib::io::output::BoolOutput;
+use algo_lib::io::output::Output;
+use algo_lib::misc::memo::memoization_vec::Memoization1d;
+use algo_lib::misc::recursive_function::Callable;
+use algo_lib::misc::test_type::TaskType;
+use algo_lib::misc::test_type::TestType;
+use algo_lib::string::str::StrReader;
+
+type PreCalc = ();
+
+fn solve(input: &mut Input, out: &mut Output, _test_case: usize, _data: &mut PreCalc) {
+    let n = input.read_size();
+    let s = input.read_str();
+    let x = input.read_long_vec(n);
+    let y = input.read_long_vec(n - 1);
+
+    let mut mem = Memoization1d::new(n + 1, |mem, pos| -> i64 {
+        if pos >= n - 1 {
+            0
+        } else {
+            let res = mem.call(pos + 1);
+            let mut cand = mem.call(pos + 2) + y[pos];
+            if s[pos] != b'R' {
+                cand -= x[pos];
+            }
+            if s[pos + 1] != b'S' {
+                cand -= x[pos + 1];
+            }
+            res.max(cand)
+        }
+    });
+    out.print_line(mem.call(0));
+}
+
+pub static TEST_TYPE: TestType = TestType::MultiNumber;
+pub static TASK_TYPE: TaskType = TaskType::Classic;
+
+pub(crate) fn run(mut input: Input, mut output: Output) -> bool {
+    eprint!("\x1B[33m\x1B[03m");
+    let mut pre_calc = ();
+    output.set_bool_output(BoolOutput::YesNo);
+
+    match TEST_TYPE {
+        TestType::Single => solve(&mut input, &mut output, 1, &mut pre_calc),
+        TestType::MultiNumber => {
+            let t = input.read();
+            for i in 1..=t {
+                solve(&mut input, &mut output, i, &mut pre_calc);
+            }
+        }
+        TestType::MultiEof => {
+            let mut i = 1;
+            while input.peek().is_some() {
+                solve(&mut input, &mut output, i, &mut pre_calc);
+                i += 1;
+            }
+        }
+        _ => {
+            unreachable!();
+        }
+    }
+    eprint!("\x1B[0m");
+    output.flush();
+    input.check_empty()
+}
+
+#[cfg(feature = "local")]
+mod tester;
+
+#[cfg(feature = "local")]
+fn main() {
+    tester::run_tests();
+}
+
+#[cfg(not(feature = "local"))]
+fn main() {
+    #[cfg(debug_assertions)]
+    eprintln!("Library code is available at https://github.com/EgorKulikov/rust_algo");
+    let input = algo_lib::io::input::Input::stdin();
+    let output = algo_lib::io::output::Output::stdout();
+    run(input, output);
+}
