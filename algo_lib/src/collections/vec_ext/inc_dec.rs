6a5a308bb27e68674d0b40d7983ca52105f30b8d
Omar Saber Talaat Abdelrahman <omar.abdelrahman.de@gmail.com>
2026-07-01 16:53:50 +0200

    (feature) inc_mut and dec_mut

--- algo_lib/src/collections/vec_ext/inc_dec.rs
+++ algo_lib/src/collections/vec_ext/inc_dec.rs
@@ -1,134 +1,64 @@
 use crate::numbers::num_traits::algebra::{AdditionMonoidWithSub, One};
 
-pub trait IncDec {
+pub trait IncDec: Sized {
+    fn inc_mut(&mut self);
+    fn dec_mut(&mut self);
+
     #[must_use]
-    fn inc(self) -> Self;
+    fn inc(mut self) -> Self {
+        self.inc_mut();
+        self
+    }
+
     #[must_use]
-    fn dec(self) -> Self;
+    fn dec(mut self) -> Self {
+        self.dec_mut();
+        self
+    }
 }
 
 impl<T: AdditionMonoidWithSub + One> IncDec for T {
-    fn inc(self) -> Self {
-        self + T::one()
+    fn inc_mut(&mut self) {
+        *self += T::one()
     }
 
-    fn dec(self) -> Self {
-        self - T::one()
-    }
-}
-
-impl<T: AdditionMonoidWithSub + One> IncDec for Vec<T> {
-    fn inc(mut self) -> Self {
-        self.iter_mut().for_each(|i| *i += T::one());
-        self
-    }
-
-    fn dec(mut self) -> Self {
-        self.iter_mut().for_each(|i| *i -= T::one());
-        self
+    fn dec_mut(&mut self) {
+        *self -= T::one()
     }
 }
 
-impl<T: AdditionMonoidWithSub + One> IncDec for Vec<Vec<T>> {
-    fn inc(mut self) -> Self {
-        self.iter_mut()
-            .for_each(|v| v.iter_mut().for_each(|i| *i += T::one()));
-        self
+impl<T: IncDec> IncDec for Vec<T> {
+    fn inc_mut(&mut self) {
+        self.iter_mut().for_each(T::inc_mut);
     }
 
-    fn dec(mut self) -> Self {
-        self.iter_mut()
-            .for_each(|v| v.iter_mut().for_each(|i| *i -= T::one()));
-        self
+    fn dec_mut(&mut self) {
+        self.iter_mut().for_each(T::dec_mut);
     }
 }
 
-impl<T: AdditionMonoidWithSub + One, U: AdditionMonoidWithSub + One> IncDec for Vec<(T, U)> {
-    fn inc(mut self) -> Self {
-        self.iter_mut().for_each(|(i, j)| {
-            *i += T::one();
-            *j += U::one();
-        });
-        self
+impl<U: IncDec, V: IncDec> IncDec for (U, V) {
+    fn inc_mut(&mut self) {
+        self.0.inc_mut();
+        self.1.inc_mut();
     }
 
-    fn dec(mut self) -> Self {
-        self.iter_mut().for_each(|(i, j)| {
-            *i -= T::one();
-            *j -= U::one();
-        });
-        self
+    fn dec_mut(&mut self) {
+        self.0.dec_mut();
+        self.1.dec_mut();
     }
 }
 
-impl<T: AdditionMonoidWithSub + One, U: AdditionMonoidWithSub + One, V> IncDec for Vec<(T, U, V)> {
-    fn inc(mut self) -> Self {
-        self.iter_mut().for_each(|(i, j, _)| {
-            *i += T::one();
-            *j += U::one();
-        });
-        self
+impl<T: IncDec, U: IncDec, V: IncDec> IncDec for (T, U, V) {
+    fn inc_mut(&mut self) {
+        self.0.inc_mut();
+        self.1.inc_mut();
+        self.2.inc_mut();
     }
 
-    fn dec(mut self) -> Self {
-        self.iter_mut().for_each(|(i, j, _)| {
-            *i -= T::one();
-            *j -= U::one();
-        });
-        self
-    }
-}
-
-impl<T: AdditionMonoidWithSub + One, U: AdditionMonoidWithSub + One, V, W> IncDec
-    for Vec<(T, U, V, W)>
-{
-    fn inc(mut self) -> Self {
-        self.iter_mut().for_each(|(i, j, ..)| {
-            *i += T::one();
-            *j += U::one();
-        });
-        self
-    }
-
-    fn dec(mut self) -> Self {
-        self.iter_mut().for_each(|(i, j, ..)| {
-            *i -= T::one();
-            *j -= U::one();
-        });
-        self
-    }
-}
-
-impl<T: AdditionMonoidWithSub + One, U: AdditionMonoidWithSub + One, V, W, X> IncDec
-    for Vec<(T, U, V, W, X)>
-{
-    fn inc(mut self) -> Self {
-        self.iter_mut().for_each(|(i, j, ..)| {
-            *i += T::one();
-            *j += U::one();
-        });
-        self
-    }
-
-    fn dec(mut self) -> Self {
-        self.iter_mut().for_each(|(i, j, ..)| {
-            *i -= T::one();
-            *j -= U::one();
-        });
-        self
-    }
-}
-
-impl<T: AdditionMonoidWithSub + One, U: AdditionMonoidWithSub + One> IncDec for (T, U) {
-    fn inc(mut self) -> Self {
-        self.0 += T::one();
-        self.1 += U::one();
-        self
-    }
-
-    fn dec(mut self) -> Self {
-        self.0 -= T::one();
-        self.1 -= U::one();
-        self
+    fn dec_mut(&mut self) {
+        self.0.dec_mut();
+        self.1.dec_mut();
+        self.2.dec_mut();
     }
 }
