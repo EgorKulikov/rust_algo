5fbe75aa2b4281280d00e10de58049229bf7f451
Egor Kulikov <egor@egork.net>
2026-07-21 13:20:06 +0200

    (contest) AtCoder Weekday Contest 0117 Beta

---
+++ archive/2026/07/2026.07.21 - AtCoder Weekday Contest 0117 Beta/b_champagne_tower.rs
@@ -0,0 +1,87 @@
+use algo_lib::collections::slice_ext::bounds::Bounds;
+use algo_lib::io::input::Input;
+use algo_lib::io::output::BoolOutput;
+use algo_lib::io::output::Output;
+use algo_lib::misc::test_type::TaskType;
+use algo_lib::misc::test_type::TestType;
+use algo_lib::numbers::num_utils::PartialSums;
+
+type PreCalc = ();
+
+fn solve(input: &mut Input, out: &mut Output, _test_case: usize, _data: &mut PreCalc) {
+    let n = input.read_size();
+    let q = input.read_size();
+    let c = input.read_long_vec(n);
+
+    let s = c.partial_sums();
+    let mut cur = 0;
+    for _ in 0..q {
+        let t = input.read_int();
+        match t {
+            1 => {
+                cur += input.read_long();
+            }
+            2 => {
+                let k = input.read_size() - 1;
+                let pos = s.upper_bound(&cur) - 1;
+                if pos > k {
+                    out.print_line(c[k]);
+                } else if pos < k {
+                    out.print_line(0);
+                } else {
+                    out.print_line(cur - s[k]);
+                }
+            }
+            _ => unreachable!(),
+        }
+    }
+}
+
+pub static TEST_TYPE: TestType = TestType::Single;
+pub static TASK_TYPE: TaskType = TaskType::Classic;
+
+pub(crate) fn run(mut input: Input, mut output: Output) -> bool {
+    eprint!("\x1B[33m\x1B[03m");
+    let mut pre_calc = ();
+    output.set_bool_output(BoolOutput::YesNo);
+
+    match TEST_TYPE {
+        TestType::Single => solve(&mut input, &mut output, 1, &mut pre_calc),
+        TestType::MultiNumber => {
+            let t = input.read();
+            for i in 1..=t {
+                solve(&mut input, &mut output, i, &mut pre_calc);
+            }
+        }
+        TestType::MultiEof => {
+            let mut i = 1;
+            while input.peek().is_some() {
+                solve(&mut input, &mut output, i, &mut pre_calc);
+                i += 1;
+            }
+        }
+        _ => {
+            unreachable!();
+        }
+    }
+    eprint!("\x1B[0m");
+    output.flush();
+    input.check_empty()
+}
+
+#[cfg(feature = "local")]
+mod tester;
+
+#[cfg(feature = "local")]
+fn main() {
+    tester::run_tests();
+}
+
+#[cfg(not(feature = "local"))]
+fn main() {
+    #[cfg(debug_assertions)]
+    eprintln!("Library code is available at https://github.com/EgorKulikov/rust_algo");
+    let input = algo_lib::io::input::Input::stdin();
+    let output = algo_lib::io::output::Output::stdout();
+    run(input, output);
+}
---
+++ archive/2026/07/2026.07.21 - AtCoder Weekday Contest 0117 Beta/e_modular_inverse_points_of_products.rs
@@ -0,0 +1,86 @@
+use algo_lib::collections::vec_ext::gen_vec::VecGen;
+use algo_lib::dynamic_value;
+use algo_lib::io::input::Input;
+use algo_lib::io::output::BoolOutput;
+use algo_lib::io::output::Output;
+use algo_lib::misc::memo::memoization_2d::Memoization2d;
+use algo_lib::misc::recursive_function::Callable2;
+use algo_lib::misc::test_type::TaskType;
+use algo_lib::misc::test_type::TestType;
+use algo_lib::numbers::mod_int::ModInt;
+use algo_lib::numbers::num_traits::algebra::{One, Zero};
+use algo_lib::numbers::num_traits::invertible::Invertible;
+
+type PreCalc = ();
+
+fn solve(input: &mut Input, out: &mut Output, _test_case: usize, _data: &mut PreCalc) {
+    let n = input.read_size();
+    let k = input.read_size();
+    let m = input.read_unsigned();
+    let a = input.read_unsigned_vec(n);
+
+    dynamic_value!(Modulo: u32 = m);
+    type Mod = ModInt<Modulo>;
+    let b = Vec::with_gen(n, |i| Mod::from(a[i]).inv().unwrap_or(Mod::zero()));
+    let mut mem = Memoization2d::new(n + 1, k + 1, |mem, pos, rem| -> Mod {
+        if pos == n {
+            if rem == 0 { Mod::one() } else { Mod::zero() }
+        } else {
+            let mut res = mem.call(pos + 1, rem);
+            if rem > 0 {
+                res += mem.call(pos + 1, rem - 1) * b[pos];
+            }
+            res
+        }
+    });
+    out.print_line(mem.call(0, k));
+}
+
+pub static TEST_TYPE: TestType = TestType::Single;
+pub static TASK_TYPE: TaskType = TaskType::Classic;
+
+pub(crate) fn run(mut input: Input, mut output: Output) -> bool {
+    eprint!("\x1B[33m\x1B[03m");
+    let mut pre_calc = ();
+    output.set_bool_output(BoolOutput::YesNo);
+
+    match TEST_TYPE {
+        TestType::Single => solve(&mut input, &mut output, 1, &mut pre_calc),
+        TestType::MultiNumber => {
+            let t = input.read();
+            for i in 1..=t {
+                solve(&mut input, &mut output, i, &mut pre_calc);
+            }
+        }
+        TestType::MultiEof => {
+            let mut i = 1;
+            while input.peek().is_some() {
+                solve(&mut input, &mut output, i, &mut pre_calc);
+                i += 1;
+            }
+        }
+        _ => {
+            unreachable!();
+        }
+    }
+    eprint!("\x1B[0m");
+    output.flush();
+    input.check_empty()
+}
+
+#[cfg(feature = "local")]
+mod tester;
+
+#[cfg(feature = "local")]
+fn main() {
+    tester::run_tests();
+}
+
+#[cfg(not(feature = "local"))]
+fn main() {
+    #[cfg(debug_assertions)]
+    eprintln!("Library code is available at https://github.com/EgorKulikov/rust_algo");
+    let input = algo_lib::io::input::Input::stdin();
+    let output = algo_lib::io::output::Output::stdout();
+    run(input, output);
+}
