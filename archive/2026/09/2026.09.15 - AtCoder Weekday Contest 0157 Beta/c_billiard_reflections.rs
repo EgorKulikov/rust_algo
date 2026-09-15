755fccaf9273aaad103ce2bbf9b065260ab8f1da
Egor Kulikov <egor@egork.net>
2026-09-15 13:16:31 +0200

    (contest) AtCoder Weekday Contest 0157 Beta

---
+++ archive/2026/09/2026.09.15 - AtCoder Weekday Contest 0157 Beta/c_billiard_reflections.rs
@@ -0,0 +1,90 @@
+use algo_lib::io::input::Input;
+use algo_lib::io::output::BoolOutput;
+use algo_lib::io::output::Output;
+use algo_lib::misc::test_type::TaskType;
+use algo_lib::misc::test_type::TestType;
+
+type PreCalc = ();
+
+fn solve(input: &mut Input, out: &mut Output, _test_case: usize, _data: &mut PreCalc) {
+    let n = input.read_size();
+    let m = input.read_long();
+    let mut s = input.read_long();
+
+    let mut qty = 0;
+    for _ in 0..n {
+        let c = input.read_char();
+        let a = input.read_long();
+        let next = if c == b'L' {
+            s - a
+        } else {
+            s + a
+        };
+        if next > m {
+            qty += (next - 1) / m;
+            s = next % (2 * m);
+            if s > m {
+                s = 2 * m - s;
+            }
+        } else if next < 0 {
+            qty += (-next - 1) / m + 1;
+            s = (-next) % (2 * m);
+            if s > m {
+                s = 2 * m - s;
+            }
+        } else {
+            s = next;
+        }
+    }
+    out.print_line((s, qty));
+}
+
+pub static TEST_TYPE: TestType = TestType::Single;
+pub static TASK_TYPE: TaskType = TaskType::Classic;
+
+pub(crate) fn run(mut input: Input, mut output: Output) -> bool {
+    eprint!("\x1B[33m\x1B[03m");
+    let mut pre_calc = ();
+    output.set_bool_output(BoolOutput::YesNo);
+
+    match TEST_TYPE {
+        TestType::Single => solve(&mut input, &mut output, 1, &mut pre_calc),
+        TestType::MultiNumber => {
+            let t = input.read();
+            for i in 1..=t {
+                solve(&mut input, &mut output, i, &mut pre_calc);
+            }
+        }
+        TestType::MultiEof => {
+            let mut i = 1;
+            while input.peek().is_some() {
+                solve(&mut input, &mut output, i, &mut pre_calc);
+                i += 1;
+            }
+        }
+        _ => {
+            unreachable!();
+        }
+    }
+    eprint!("\x1B[0m");
+    output.flush();
+    input.check_empty()
+}
+
+
+#[cfg(feature = "local")]
+mod tester;
+
+#[cfg(feature = "local")]
+fn main() {
+    tester::run_tests();
+}
+
+#[cfg(not(feature = "local"))]
+fn main() {
+    #[cfg(debug_assertions)]
+    eprintln!("Library code is available at https://github.com/EgorKulikov/rust_algo");
+    let input = algo_lib::io::input::Input::stdin();
+    let output = algo_lib::io::output::Output::stdout();
+    run(input, output);
+}
